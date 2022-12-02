ee10620a3f0a9da7d79e553cbc4b27fc48b997a4
Kritika Saxena <saxenakritika717@gmail.com>
2022-12-02 17:01:28 +0530

    update the pitchcontroller to use json

--- src/controllers/pitch.controller.js
+++ src/controllers/pitch.controller.js
@@ -2,12 +2,17 @@ const Pitch = require("../models/pitches.model");
 const Offer = require("../models/offer.model");
 
 exports.createPitch = (req, res) => {
+  if (req.body.equity > 100)
+    res.status(400).json({
+      'msg': 'Equity should be less than 100'
+    })
+
   const newPitch = new Pitch({ ...req.body });
   newPitch.save((err, response) => {
     if (err) {
-      res.status(400).send("Invalid Request Body");
+      res.status(400).json("Invalid Request Body");
     } else {
-      res.status(201).send({
+      res.status(201).json({
         id: response.id,
       });
     }
@@ -19,7 +24,7 @@ exports.createOfferForPitch = (req, res) => {
 
   newOffer.save((err, response) => {
     if (err) {
-      res.status(400).send("Invalid Request Body");
+      res.status(400).json({ err: "Invalid Request Body" });
     }
   });
 
@@ -35,9 +40,9 @@ exports.createOfferForPitch = (req, res) => {
   };
   Pitch.updateOne(queryString, updateDocument, (err, response) => {
     if (err) {
-      res.status(404).send("Pitch Not Found");
+      res.status(404).json("Pitch Not Found");
     } else {
-      res.status(201).send({
+      res.status(201).json({
         id: newOffer._id,
       });
     }
@@ -50,18 +55,39 @@ exports.getAllPitches = (req, res) => {
   Pitch.find({}, projectionValues)
     .sort({ createdAt: -1 })
     .then((pitches) => {
-      let response = [];
 
+      if (pitches.length == 0) {
+        res.json([])
+        return
+      }
+
+      let response = []
       pitches.forEach((pitch) => {
         const { _id: id, ...pitchData } = pitch._doc;
-        response = [...response, { id, ...pitchData }];
+        response = [...response, {
+          id: id,
+          entrepreneur: pitchData.entrepreneur,
+          pitchTitle: pitchData.pitchTitle,
+          pitchIdea: pitchData.pitchIdea,
+          askAmount: pitchData.askAmount,
+          equity: pitchData.equity,
+          offers: pitchData.offers.map(offer => {
+            return {
+              id: offer.id,
+              investor: offer.investor,
+              comment: offer.comment,
+              amount: offer.amount,
+              equity: offer.equity,
+            }
+          }
+          )
+        }];
       });
-      res.status(200).send(response);
+      res.status(200).json(response)
     })
     .catch((err) => {
-      res.status(404).send({
+      res.status(404).json({
         message: "Cannot retrieve the pitches collection",
-        err,
       });
     });
 };
@@ -73,10 +99,30 @@ exports.getPitchById = async (req, res) => {
 
   Pitch.findById(pitchId, projectionValues, (err, pitch) => {
     if (err) {
-      res.status(404).send("Pitch Not Found");
+      res.status(404).json("Pitch Not Found");
     } else {
-      const { _id: id, ...pitchData } = pitch._doc;
-      res.status(200).send({ id, ...pitchData });
+      if (pitch.length == 0) {
+        res.json([])
+        return
+      }
+      res.status(200).json({
+        id: pitch._id,
+        entrepreneur: pitch.entrepreneur,
+        pitchTitle: pitch.pitchTitle,
+        pitchIdea: pitch.pitchIdea,
+        askAmount: pitch.askAmount,
+        equity: pitch.equity,
+        offers: pitch.offers.map(offer => {
+          return {
+            id: offer.id,
+            investor: offer.investor,
+            comment: offer.comment,
+            amount: offer.amount,
+            equity: offer.equity,
+          }
+        }
+        )
+      })
     }
   });
 };
